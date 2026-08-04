36cf874980b09d6b1f7e41c23a9653100ad78175
Vijay Manchiraju <142843960+vmanc@users.noreply.github.com>
2026-08-04 10:53:32 -0400

    Update Character-level encode/decode (Build GPT 2/10)

--- data/vocab.py
+++ data/vocab.py
@@ -5,7 +5,8 @@ class Solution:
         # Return (stoi, itos) where:
         # - stoi maps each unique character to a unique integer (sorted alphabetically)
         # - itos is the reverse mapping (integer to character)
-        stoi = {char : i for i, char in enumerate(sorted(set(text)))}
+        unique_characters = sorted(set(text))
+        stoi = {char : i for i, char in enumerate(unique_characters)}
         itos = {i : char for i, char in stoi.items()}
         return (stoi, itos)
 
